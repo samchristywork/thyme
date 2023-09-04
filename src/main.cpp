283509a88ed02b15973e145bdea3bc80fe2d7464
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-09-03 20:38:15 -0500

    feat: Add simple program
    This program just collects arguments to be sent to the child process,
    which isn't implemented yet.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << " command [args...]" << endl;
+    return EXIT_FAILURE;
+  }
+
+  // Collect arguments to be passed to child
+  vector<char *> childArgs;
+  for (int i = 1; i < argc; ++i) {
+    childArgs.push_back(argv[i]);
+  }
+  childArgs.push_back(NULL);
+}
